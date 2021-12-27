b2e34597667b690d8599d1157a9d6246149877d2
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-26 23:15:33 -0500

    1er commit

---
+++ src/main/java/com/example/githubactiongradle/GithubActionGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactiongradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionGradleApplication.class, args);
+	}
+
+}
